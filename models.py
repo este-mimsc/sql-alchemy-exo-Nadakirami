b654cfc08e353925c3d52f504a220d86c86d8248
Nadakirami <kiraminada39@gmail.com>
2025-11-26 19:32:06 +0100

    Update models.py
    i add the modification

--- models.py
+++ models.py
@@ -14,7 +14,9 @@ class User(db.Model):
     # TODO: Add id primary key, username (unique + required), and
     # a relationship to ``Post`` named ``posts``.
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)  # Students should customize constraints
+    username = db.Column(db.String(80),unique=True, nullable=False)
+    posts = db.relationship("Post", back_populates="author", lazy=True)
+  # Students should customize constraints
 
     def __repr__(self):  # pragma: no cover - convenience repr
         return f"<User {getattr(self, 'username', None)}>"
@@ -28,9 +30,11 @@ class Post(db.Model):
     # TODO: Add id primary key, title, content, foreign key to users.id,
     # and a relationship back to the ``User`` model.
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    content = db.Column(db.Text)
-    user_id = db.Column(db.Integer)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text,nullable=false)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"), nullable=False)
+    author = db.relationship("User", back_populates="posts")
+
 
     def __repr__(self):  # pragma: no cover - convenience repr
         return f"<Post {getattr(self, 'title', None)}>"
